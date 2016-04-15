d126cb4ee6174e5f4b41fc2a31fb60fe449ec937
Gerjan Konterman <gkonterman@sintrum.nl>
2016-04-15 14:46:30 +0200

    Version bump

--- scripts/publish.cmd
+++ scripts/publish.cmd
@@ -8,5 +8,9 @@ call npm install -g vsce
 echo Done
 
 echo Publishing to marketplace...
-call vsce publish
+call vsce publish patch :: Default to patch update
+echo Done
+
+echo Committing version bump...
+call git commit -am "Version bump"
 echo Done
